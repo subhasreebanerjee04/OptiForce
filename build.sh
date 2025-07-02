9e06e823ffdcaa0b2162484680d49e59c8d79c73
Subhasree Banerjee <sbanerjeework04@gmail.com>
2025-07-02 23:48:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@
 # Set Python to 3.12
 python3.12 -m venv .venv
 source .venv/bin/activate
-
+# Add to build.sh
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 # Install with no cache
 pip install --upgrade pip
 pip install -r requirements.txt --no-cache-dir

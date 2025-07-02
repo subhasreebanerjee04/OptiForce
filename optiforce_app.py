3ab812ecbfd4436aa00896edef9ac19975bf7757
Subhasree Banerjee <sbanerjeework04@gmail.com>
2025-07-03 00:43:08 +0530

    Update optiforce_app.py

--- optiforce_app.py
+++ optiforce_app.py
@@ -572,11 +572,11 @@ class LightweightLLMService:
         os.environ["HF_HUB_DISABLE_EXPERIMENTAL_WARNING"] = "1"
         
         self.tokenizer = AutoTokenizer.from_pretrained(
-            "google/flan-t5-small",
+            "microsoft/Phi-3-mini-4k-instruct",
             trust_remote_code=True
         )
         self.model = AutoModelForCausalLM.from_pretrained(
-            "google/flan-t5-small",
+            "microsoft/Phi-3-mini-4k-instruct",
             torch_dtype=torch.float32,
             trust_remote_code=True
         )
